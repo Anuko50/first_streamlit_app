a643ea05f022667b3520ad26f34dd38e8de34ede
Anuko50 <43858498+Anuko50@users.noreply.github.com>
2023-03-21 09:17:07 +0100

    Update streamlit_app.py
    solved the copy-paste error hehe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents new healthy dinner')
 streamlit.header('Breakfast Menu:')
-streamlit..text('Omega & Blueberry Oatmeal')
-streamlit..text('Kale, Spinach & Rocket Smoothie')
-streamlit..text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
