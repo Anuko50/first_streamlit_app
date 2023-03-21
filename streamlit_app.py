0803248e376ca506086b3c159ce39ff20b43809a
Anuko50 <43858498+Anuko50@users.noreply.github.com>
2023-03-21 15:10:09 +0100

    Update streamlit_app.py
    now we make querys with the python package request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 # First steps: adding text and that kind of stuff
 
@@ -30,5 +31,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#now we make querys with the python package request
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
