b84a11599debc43a27db617e08a686ea6fa4d4ab
Anuko50 <43858498+Anuko50@users.noreply.github.com>
2023-03-21 15:14:07 +0100

    Update streamlit_app.py
    added the normalized .json as a dataframe and show it in streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #now we make querys with the python package request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())  # just writes data to the screen
+
+# we take the json and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# we put the output as a dataframe table
+streamlit.dataframe(fruityvice_normalized)
 
 
